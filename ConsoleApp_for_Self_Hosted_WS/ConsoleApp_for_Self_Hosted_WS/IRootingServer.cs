183aef121d21feaf253f674b139444c482d5a768
Arnaud Avocat Gros <arnaud.avocatgros18@gmail.com>
2023-11-22 17:52:00 +0100

    soap server can get location

---
+++ ConsoleApp_for_Self_Hosted_WS/ConsoleApp_for_Self_Hosted_WS/IRootingServer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// add assembly System.ServiceModel  and using for the corresponding model
+using System.ServiceModel;
+
+namespace ConsoleApp_for_Rooting_Server
+{
+
+    [ServiceContract()]
+    public interface IRootingServer
+    {
+        [OperationContract()]
+        void getItinerary(string start, string end);
+    }
+
+
+}
---
+++ ConsoleApp_for_Self_Hosted_WS/ConsoleApp_for_Self_Hosted_WS/RootingServer.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_for_Rooting_Server
+{
+    class RootingServer : IRootingServer
+    {
+        public async void getItinerary(string start, string end)
+        {
+            // etape 1: regarder le trajet de start a end a pied pour s'en servir comme base
+            // appel a l'api nominatim pour trouver start et end
+            string nominatimApi = "https://nominatim.openstreetmap.org";
+            string getStartLocation = "/search?q=" + start + "&format=json&polygon_kml=1&addressdetails=1";
+            string getEndLocation = "/search?q=" + end + "&format=json&polygon_kml=1&addressdetails=1";
+
+            HttpClient client = new HttpClient();
+            HttpRequestMessage requestStart = new HttpRequestMessage(HttpMethod.Get, nominatimApi + getStartLocation);
+            HttpRequestMessage requestEnd = new HttpRequestMessage(HttpMethod.Get, nominatimApi + getEndLocation);
+
+            // Ajouter le referer dans les en-têtes
+            requestStart.Headers.Referrer = new Uri("https://facebook.com");
+            requestEnd.Headers.Referrer = new Uri("https://facebook.com");
+
+            // Envoyer les requêtes et attendre les réponses
+            Task<HttpResponseMessage> responseStart = client.SendAsync(requestStart);
+            Task<HttpResponseMessage> responseEnd = client.SendAsync(requestEnd);
+
+            Task.WaitAll(responseStart, responseEnd);
+
+            // Lire et afficher le contenu des réponses
+            string contentStart = await responseStart.Result.Content.ReadAsStringAsync();
+            string contentEnd = await responseEnd.Result.Content.ReadAsStringAsync();
+
+            Console.WriteLine("Start point location:");
+            Console.WriteLine(getLocation(contentStart));
+            Console.WriteLine("End point location:");
+            Console.WriteLine(getLocation(contentEnd));
+
+            // appel a l'api openrouteservice pour trouver le trajet
+            // etape 2: trouver la station de velo la plus proche de start et celle plus proche de end
+            // dans toutes les stations du contrat trouver les x plus proches a vol d'oiseau de start et celles de end (appel api jcdecaux ou recherche dans cache)
+            // dans les x stations trouvées, calculer le trajet a pied le plus proches de start et de end 
+            // etape 3: trouver la somme des trajets start->station_start->station_end->end
+            // trouver le trajet a velo station_start->station_end et ajouter les trajets a pieds
+            // etape 4: comparer etape 1 et etape 3
+            // retourner le meilleur trajet
+        }
+
+        public string getLocation(string content)
+        {
+            var locationData = JsonConvert.DeserializeObject<LocationData[]>(content);
+            string res = locationData[0].lon + "," + locationData[0].lat;
+            return res;
+        }
+    }
+    public class LocationData
+    {
+        public string lat { get; set; }
+        public string lon { get; set; }
+    }
+
+    public class Itinerary
+    {
+        //a definir selon le type de carte dans le client
+    }
+
+}
